04f181a54146a9e88226ad5e27d03b796d265d05
naraelee <nar.lee@mail.utoronto.ca>
2023-01-22 01:42:01 -0500

    hellooo

---
+++ src/pages/Tutorial.js
@@ -0,0 +1,23 @@
+import HomeHeader from "./components/HomeHeader";
+import Home from "/Users/naraelee/Desktop/con/Gsoft-Boarding/src/pages/Home/Home.js";
+
+const Home = () => {
+    return (
+
+        <>
+        
+        <HomeHeader/>
+
+        
+
+
+
+
+
+
+        
+        
+        </>
+
+    )
+}
