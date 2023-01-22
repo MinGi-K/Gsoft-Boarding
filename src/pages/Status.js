1f10531fed9c4b851f58c86ad91b6a43e4f4dc00
oshley213 <oshley0103@gmail.com>
2023-01-22 08:39:47 -0500

    status page done

--- src/pages/Status.js
+++ src/pages/Status.js
@@ -8,6 +8,7 @@ import IconButton from '@mui/material/IconButton';
 import { Form } from "react-router-dom";
 import { Label } from "@mui/icons-material";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import StarIcon from '@mui/icons-material/Star';
 import { Slider } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
@@ -68,29 +69,65 @@ const Status = () => {
                     Get to know and inform your community how happy you are working with other softers.
                 </Typography>
             </Box>
-            <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center', paddingTop: 3,
+            <Box sx={{ display: 'grid', alignItems: 'center', textAlign: 'left', paddingTop: 3,
             paddingLeft: 15, paddingRight: 15, backgroundColor: '#E49E7A', }}>
                 <Typography sx={{color: 'black', fontSize: 'xxx-large'}}>Thank you for sharing your status today !</Typography>
             </Box>
-            {/* <Box sx={{display: 'grid', alignItems: 'center', textAlign: 'center', gridTemplateColumns: 'repeat(3, 1fr)',
-                    paddingTop: 3, borderBottom: 10, paddingBottom: 3, marginLeft: 15, marginRight: 15,
-                    borderColor: 'black'}}> */}
-                    {/* <Box sx={{width: 400}}>
-                        <Typography sx = {{color: 'black'}} gutterBottom>Feeling</Typography>
+            <Box sx={{display: 'grid', alignItems: 'center', textAlign: 'center', gridTemplateColumns: 'repeat(3, 1fr)',
+                    paddingTop: 3,  paddingBottom: 3, paddingLeft: 15, paddingRight: 15, borderColor: 'black'}}>
+                    <Box sx={{width: 300, textAlign: 'left', alignItems: 'center'}}>
+                        <Typography sx={{color: 'black', fontSize: "large", paddingBottom: 3}}>Your Status Today</Typography>
+                        <Typography sx = {{color: 'black', fontSize: 'small'}} gutterBottom>Feeling</Typography>
                             <PrettoSlider
                                 valueLabelDisplay="auto"
                                 aria-label="pretto slider"
-                                defaultValue={20}
+                                defaultValue={70}
                             />
-                        <Typography sx = {{color: 'black'}} gutterBottom>Goal</Typography>
+                        <Typography sx = {{color: 'black', fontSize: 'small'}} gutterBottom>Goal</Typography>
                             <PrettoSlider
                                 valueLabelDisplay="auto"
                                 aria-label="pretto slider"
-                                defaultValue={80}
+                                defaultValue={30}
                             />
-                    </Box> */}
+                    </Box>
+                    
+                    <Box sx={{minwidth: 300, textAlign: 'center', borderRight: 2, borderLeft: 2, color: 'black'}}>
+                        <Typography sx = {{color: 'black', fontSize: 'large'}}>You are feeling</Typography>
+                        <Typography sx = {{color: 'black', fontSize: 'xx-large', fontWeight: "bold", marginTop: 2, marginBottom: 2}}>49%</Typography>
+                        <Typography sx = {{color: 'black', fontSize: 'large'}}>better than last week!</Typography>
+                    </Box>
 
-                {/* </Box> */}
+                    <Box sx={{minwidth: 300, textAlign: 'center', color: 'black'}}>
+                        <Typography sx = {{color: 'black', fontSize: 'large'}}>You have achieved</Typography>
+                        <Typography sx = {{color: 'black', fontSize: 'xxx-large', fontWeight: "bold", marginTop: 2, marginBottom: 2}}>89%</Typography>
+                        <Typography sx = {{color: 'black', fontSize: 'large'}}>of your goal this year so far !</Typography>
+                    </Box>
+
+
+                </Box>
+
+                <Box sx={{display: 'grid', alignItems: 'center', textAlign: 'left', paddingTop: 3,  paddingBottom: 3, 
+                paddingLeft: 15, paddingRight: 15, borderColor: 'black'}}>
+                    <MenuItem sx={{color: 'black', fontSize: "large"}}>
+                        <ListItemIcon>
+                            <StarIcon sx={{ color: 'black', fontSize: "xxx-large", paddingRight: 1 }} />
+                        </ListItemIcon>
+                        We are happy that you are feeling well with us:)
+                    </MenuItem>
+                    <Typography sx={{color: 'black', fontSize: "large", paddingLeft: 8}}>We are assuming the JAM SESSION you joined yesterday boosted your happiness?! </Typography>
+                </Box>
+
+                <Box sx={{display: 'grid', alignItems: 'right', textAlign: 'right', paddingTop: 5, paddingLeft: 114, paddingRight: 15}}>
+                    <Box sx={{display: 'flex', alignItems: 'right', textAlign: 'right', justifyContent: 'space-between'}}>
+                        <Typography sx={{color: 'black', fontSize: "large", paddingLeft: 8}}>We are so proud of you John !</Typography>
+                        <StarIcon sx={{ color: 'black', fontSize: "xxx-large", paddingRight: 1, alignItems: 'right', textAlign: 'right' }} />    
+                    </Box>
+                </Box>
+
+                <Box sx={{display: 'grid', alignItems: 'right', textAlign: 'right', paddingLeft: 15, paddingRight: 21.5, paddingBottom: 20}}>
+                    <Typography sx={{color: 'black', fontSize: "large", paddingLeft: 8}}>We will stay here to support you till you reach your dream in GSoft :)</Typography>
+                </Box>
+
         </Box>
 
         
